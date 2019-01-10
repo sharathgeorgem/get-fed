45b9ef864005c6b017c07ff42fdce668a8bfbc89
Sharath George M <sharathgeorgem@gmail.com>
2019-01-10 11:08:57 +0530

    Created shared Layout and added Bootstrap import for global usage

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button, Alert } from 'reactstrap'
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <Alert color='primary'>
+          Wanna get fed?
+        </Alert>
+        &nbsp; <Button color='primary'>Click Me</Button>
+      </div>
+    </div>
+  )
+}
