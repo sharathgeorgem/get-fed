2eaccf0ed4d656512bcd7ab318b96dbbca9964eb
Sharath George M <sharathgeorgem@gmail.com>
2019-01-31 11:56:00 +0530

    Removed env and next.config files

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicRuntimeConfig: { // Will be available on both server and client
-    domain: 'http://localhost:3000',
-    database: 'mongodb+srv://admin:chwiggy@get-fed-2q8ne.mongodb.net/test?retryWrites=true',
-    mapquestKey: 'GFf1LiiyRhj0zRd4UBrVzlmr7fUOjUvu'
-  }
-}
