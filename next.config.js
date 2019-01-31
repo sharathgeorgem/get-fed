cd934587aba13ffd68b1149888499f608ffb742a
Sharath George M <sharathgeorgem@gmail.com>
2019-01-31 10:15:06 +0530

    Added next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicRuntimeConfig: { // Will be available on both server and client
+    domain: 'http://localhost:3000',
+    database: 'mongodb+srv://admin:chwiggy@get-fed-2q8ne.mongodb.net/test?retryWrites=true',
+    mapquestKey: 'GFf1LiiyRhj0zRd4UBrVzlmr7fUOjUvu'
+  }
+}
